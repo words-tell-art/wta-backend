906633171be3312ab557c842cc9e70f156696f4b
Draym <kevin.draym@gmail.com>
2023-03-24 23:01:34 +0800

    task: add testnet cicd flow

--- src/config/blockchain.config.ts
+++ src/config/blockchain.config.ts
@@ -2,7 +2,7 @@ import * as process from "process"
 
 const blockchainConfig = {
     ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY!,
-    NETWORK: process.env.NETWORK!,
+    NETWORK: process.env.ETH_NETWORK!,
     CONTRACT_ART_ADDRESS: process.env.CONTRACT_ART_ADDRESS!,
     CRAFT_LAUNCH_DATE: process.env.CRAFT_LAUNCH_DATE!,
     ART_LOADING_URL: process.env.ART_LOADING_URL!,
