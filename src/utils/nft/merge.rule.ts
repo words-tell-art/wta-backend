8538bef049dc2de3db637a5c4d00f7bd0deee85f
Draym <kevin.draym@gmail.com>
2023-04-06 20:09:35 +0800

    task: handle multiple hue props and new art merge hue rule

--- src/utils/nft/merge.rule.ts
+++ src/utils/nft/merge.rule.ts
@@ -1,8 +1,7 @@
-import {isEmpty, merge, throwIf} from "@d-lab/common-kit"
+import {isEmpty, isNotEmpty, merge, throwIf} from "@d-lab/common-kit"
 import Errors from "../errors/Errors"
 import {Art, Word} from "../../interfaces"
 import {colorHex, hexToRgb, RGB} from "../colors"
-import * as console from "console"
 
 export type MetadataProps = { [key: string]: string | number | boolean | Date | undefined }
 
@@ -26,6 +25,25 @@ export function mergeColors(colors: string[]): RGB | null {
     return result
 }
 
+function mergeHues(hue1: string, hue2: string, hue3: string, hue4: string, hue5: string, hue6: string, hue7: string, hue8: string): { hue1: string, hue2: string, hue3: string, hue4: string } {
+    const priority: string[] = [hue1, hue5, hue2, hue6, hue3, hue7, hue4, hue8]
+    let current = 0
+    const result = ["", "", "", ""]
+    for (const hue of priority) {
+        if (current === 3) break
+        if (isNotEmpty(hue)) {
+            result[current] = hue
+            current++
+        }
+    }
+    return {
+        hue1: result[0],
+        hue2: result[1],
+        hue3: result[2],
+        hue4: result[3],
+    }
+}
+
 export function craftArt(words: Word[], ids: number[]): MergedNFT {
     throwIf(words.length < 2, Errors.INVALID_ART_Craft(`ArtCraft: only ${words.length} found from [${JSON.stringify(ids)}].`))
     let props = {}
@@ -39,13 +57,10 @@ export function craftArt(words: Word[], ids: number[]): MergedNFT {
         }, true)
     }
     const hues = words.map(word => word.metadata.properties.hue)
-    if (hues.length < 4) {
-        props["hue1"] = mergeColors(hues)?.hue()
-        props["hue2"] = ""
-    } else {
-        props["hue1"] = mergeColors(hues.slice(0, 2))?.hue()
-        props["hue2"] = mergeColors(hues.slice(3))?.hue()
-    }
+    props["hue1"] = mergeColors(hues)?.hue()
+    props["hue2"] = ""
+    props["hue3"] = ""
+    props["hue4"] = ""
     return {
         words: [
             props["w1"],
@@ -56,7 +71,7 @@ export function craftArt(words: Word[], ids: number[]): MergedNFT {
         ],
         image: null,
         properties: props,
-        hues: [props["hue1"]]
+        hues: [props["hue1"], props["hue2"], props["hue3"], props["hue4"]]
     }
 }
 
@@ -64,14 +79,22 @@ export function mergeArt(arts: Art[], ids: number[]): MergedNFT {
     throwIf(arts.length != 2, Errors.INVALID_ART_Craft(`ArtMerge: one art hasn't been found from [${JSON.stringify(ids)}].`))
     const props = {
         ...arts[0].metadata.properties,
-        w1: arts[0].metadata.properties["w1"] || arts[1].metadata.properties["w1"],
-        w2: arts[1].metadata.properties["w2"] || arts[0].metadata.properties["w2"],
-        w3: arts[0].metadata.properties["w3"] || arts[1].metadata.properties["w3"],
-        w4: arts[1].metadata.properties["w4"] || arts[0].metadata.properties["w4"],
-        w5: arts[0].metadata.properties["w5"] || arts[1].metadata.properties["w5"],
+        w1: isNotEmpty(arts[0].metadata.properties["w1"]) ? arts[0].metadata.properties["w1"] : arts[1].metadata.properties["w1"],
+        w2: isNotEmpty(arts[1].metadata.properties["w2"]) ? arts[1].metadata.properties["w2"] : arts[0].metadata.properties["w2"],
+        w3: isNotEmpty(arts[0].metadata.properties["w3"]) ? arts[0].metadata.properties["w3"] : arts[1].metadata.properties["w3"],
+        w4: isNotEmpty(arts[1].metadata.properties["w4"]) ? arts[1].metadata.properties["w4"] : arts[0].metadata.properties["w4"],
+        w5: isNotEmpty(arts[0].metadata.properties["w5"]) ? arts[0].metadata.properties["w5"] : arts[1].metadata.properties["w5"],
         generation: (arts[0].metadata.properties["generation"] > arts[1].metadata.properties["generation"] ? arts[0].metadata.properties["generation"] : arts[1].metadata.properties["generation"]) + 1,
-        hue1: arts[0].metadata.properties["hue1"],
-        hue2: arts[1].metadata.properties["hue1"]
+        ...mergeHues(
+            arts[0].metadata.properties["hue1"],
+            arts[0].metadata.properties["hue2"],
+            arts[0].metadata.properties["hue3"],
+            arts[0].metadata.properties["hue4"],
+            arts[1].metadata.properties["hue1"],
+            arts[1].metadata.properties["hue2"],
+            arts[1].metadata.properties["hue3"],
+            arts[1].metadata.properties["hue4"]
+        )
     }
     return {
         words: [
@@ -83,6 +106,6 @@ export function mergeArt(arts: Art[], ids: number[]): MergedNFT {
         ],
         image: isEmpty(arts[1].metadata.imageUrl) ? null : arts[1].metadata.imageUrl,
         properties: props,
-        hues: [props.hue1, props.hue2]
+        hues: [props.hue1, props.hue2, props.hue3, props.hue4]
     }
 }
