3027902e1ace8a39949807c257d8d30e046f1f9e
Draym <kevin.draym@gmail.com>
2023-04-12 01:47:50 +0800

    fix: word list api query validation

--- src/routes/word.route.ts
+++ src/routes/word.route.ts
@@ -1,6 +1,6 @@
 import {Router} from "express"
 import {Endpoint} from "../enums"
-import {handle, validatePathRequest, validateRequest} from "@d-lab/api-kit"
+import {handle, validatePathRequest, validateQueryRequest, validateRequest} from "@d-lab/api-kit"
 import authMiddleware from "../middleware/auth.middleware"
 import hasRole from "../middleware/has-role.middleware"
 import {Role} from "@d-lab/sso"
@@ -11,7 +11,7 @@ const router = Router()
 const ctrl = new WordController()
 
 router.get(Endpoint.WORD_Get, validatePathRequest(WordGetRequest), handle.bind(ctrl.get))
-router.get(Endpoint.WORD_List, validatePathRequest(WordListRequest), handle.bind(ctrl.list))
+router.get(Endpoint.WORD_List, validateQueryRequest(WordListRequest), handle.bind(ctrl.list))
 router.post(Endpoint.WORD_Create, authMiddleware(), hasRole(Role.Moderator), validateRequest(WordCreateRequest), handle.bind(ctrl.create))
 router.put(Endpoint.WORD_Update, authMiddleware(), hasRole(Role.Moderator), validateRequest(WordUpdateRequest), handle.bind(ctrl.update))
 
