48ead779f5fb772deca969e488143ddc953b03c5
Draym <kevin.draym@gmail.com>
2023-04-15 19:11:51 +0800

    task: create Art on art-request process

--- src/services/art-request.service.ts
+++ src/services/art-request.service.ts
@@ -1,11 +1,14 @@
 import {ArtRequestModel} from "../models"
 import RequestState from "../enums/request-state.enum"
-import {artRequestRepo} from "../repositories"
+import {artRequestRepo, chainEventRepo} from "../repositories"
 import metadataClient from "../clients/metadata.client"
 import {Blockchain} from "@d-lab/metadata"
 import blockchainConfig from "../config/blockchain.config"
 import {sequelize} from "../db/database"
 import Opensea from "../clients/opensea.client"
+import {artService} from "./index"
+import EventArguments from "../interfaces/event-arguments.interface"
+import {EventName, TokenType} from "../enums"
 
 export default class ArtRequestService {
 
@@ -37,13 +40,23 @@ export default class ArtRequestService {
                 imageUrl: imageUrl,
                 state: RequestState.PROCESSED
             }, {transaction: t})
-            await metadataClient.token.updateMetadata(
+            const metadata = await metadataClient.token.updateMetadata(
                 {chainId: Blockchain.ETHEREUM, collection: blockchainConfig.CONTRACT_ART_ADDRESS, tokenId: request.nftId.toString()},
                 {
                     imageUrl: imageUrl,
                     animationUrl: ""
                 }
             )
+            const chainEvent = await chainEventRepo.get(request.chainEventId)
+            const args: EventArguments = JSON.parse(it.arguments)
+            if (chainEvent.event === EventName.CRAFT) {
+                const burned: number[] = args.idBurned as []
+                const parentIds = [args.id, ...burned]
+                await artService.create(request.nftId, metadata, parentIds, TokenType.WORD)
+            } else {
+                const parentIds = [args.id, args.idBurned as number]
+                await artService.create(request.nftId, metadata, parentIds, TokenType.ART)
+            }
             await Opensea.syncMetadata(blockchainConfig.CONTRACT_ART_ADDRESS, request.nftId)
             return request
         })
--- src/services/art.service.ts
+++ src/services/art.service.ts
@@ -2,15 +2,13 @@ import db from "../db/database"
 import {ArtModel} from "../models"
 import {TokenType} from "../enums"
 import {eq} from "@d-lab/api-kit"
-import Errors from "../utils/errors/Errors"
 import {artRepo} from "../repositories"
-import {merge, throwIfNot} from "@d-lab/common-kit"
+import {merge} from "@d-lab/common-kit"
 import {MetadataDto, PartialMetadataRequest} from "@d-lab/metadata"
 
 export default class ArtService {
     async create(nftId: number, metadata: MetadataDto, parentIds: number[], parentType: TokenType): Promise<ArtModel> {
         if (parentType === TokenType.WORD) {
-            throwIfNot(parentIds.length < 5, Errors.INVALID_ART_Craft(`Can only craft Art with 5 words maximum, not ${parentIds.length}.`))
             return await db.Arts.create({
                 nftId: nftId,
                 metadata: metadata,
@@ -20,7 +18,6 @@ export default class ArtService {
                 artAncestors: []
             })
         } else {
-            throwIfNot(parentIds.length === 2, Errors.INVALID_ART_Craft(`Can only craft Art from 2 art, not ${parentIds.length}.`))
             const ancestor1 = await artRepo.getBy(eq({nftId: parentIds[0]}).orderDesc("id"))
             const ancestor2 = await artRepo.getBy(eq({nftId: parentIds[1]}).orderDesc("id"))
             return await db.Arts.create({
