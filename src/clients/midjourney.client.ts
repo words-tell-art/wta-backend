c01451b2411292e2dd559dddd084362af93eb9ce
Draym <kevin.draym@gmail.com>
2023-04-16 00:01:21 +0800

    task: remove input image on art merge (bad result)

--- src/clients/midjourney.client.ts
+++ src/clients/midjourney.client.ts
@@ -79,7 +79,7 @@ export default class MidjourneyClient {
         if (isNotNull(hues)) {
             colors = `colorful with ${hues!} dominant color`
         }
-        return `${isNotNull(image) ? image + " " : ""}${words}::2 style cyberpunk::1.2 film noir, minimal environment, ${colors} --no frame`
+        return `${words}::2 style cyberpunk::1.2 film noir, minimal environment, ${colors} --no frame`
     }
 
     async listen() {
